a5fe4fa5382cd2052f73b21c12aeca05dd82a841
Hannah Macadam <Hannah.Macadam22@imperial.ac.uk>
2024-12-30 21:30:21 +0000

    completed exercises

--- expressions/expressions.py
+++ expressions/expressions.py
@@ -1,9 +1,14 @@
+"""Implement operation hierarchy and differentiation for expressions."""
+
 import numbers
 from functools import singledispatch
 
+
 class Expression:
+    """Define super class expression."""
 
     def __init__(self, *operands):
+        """Initialise."""
         operands = list(operands)
         for i in range(len(operands)):
             if isinstance(operands[i], numbers.Number):
@@ -13,74 +18,96 @@ class Expression:
         self.operands = operands
 
     def __add__(self, other):
+        """Define addition of expressions."""
         if isinstance(other, numbers.Number):
             return Add(self, Number(other))
         else:
             return Add(self, other)
 
     def __radd__(self, other):
+        """Define reverse addition of expressions."""
         if isinstance(other, numbers.Number):
             return Add(Number(other), self)
         else:
-            raise TypeError(f"Unsupported operand type(s) for +: '{type(other).__name__}' and '{type(self).__name__}'")
+            raise TypeError(
+                f"Unsupported operand type(s) for +: '{type(other).__name__}' \
+                and '{type(self).__name__}'"
+                           )
 
     def __sub__(self, other):
+        """Define subtraction of expressions."""
         if isinstance(other, numbers.Number):
             return Sub(self, Number(other))
         else:
             return Sub(self, other)
 
     def __rsub__(self, other):
+        """Define reverse subtraction of expressions."""
         if isinstance(other, numbers.Number):
             return Sub(Number(other), self)
         else:
-            raise TypeError(f"Unsupported operand type(s) for -: '{type(other).__name__}' and '{type(self).__name__}'")
-
+            raise TypeError(
+                f"Unsupported operand type(s) for -: '{type(other).__name__}' \
+                and '{type(self).__name__}'")
 
     def __mul__(self, other):
+        """Define multiplication of expressions."""
         if isinstance(other, numbers.Number):
             return Mul(self, Number(other))
         else:
             return Mul(self, other)
 
     def __rmul__(self, other):
+        """Define reverse multiplication of expressions."""
         if isinstance(other, numbers.Number):
             return Mul(Number(other), self)
         else:
-            raise TypeError(f"Unsupported operand type(s) for *: '{type(other).__name__}' and '{type(self).__name__}'")
-
+            raise TypeError(
+                f"Unsupported operand type(s) for *: '{type(other).__name__}' \
+                and '{type(self).__name__}'")
 
     def __truediv__(self, other):
+        """Define division of expressions."""
         if isinstance(other, numbers.Number):
             return Div(self, Number(other))
         else:
             return Div(self, other)
 
     def __rtruediv__(self, other):
+        """Define reverse division of expressions."""
         if isinstance(other, numbers.Number):
             return Div(Number(other), self)
         else:
-            raise TypeError(f"Unsupported operand type(s) for /: '{type(other).__name__}' and '{type(self).__name__}'")
+            raise TypeError(
+                f"Unsupported operand type(s) for /: '{type(other).__name__}' \
+                and '{type(self).__name__}'")
 
     def __pow__(self, other):
+        """Define exponentiation of expressions."""
         if isinstance(other, numbers.Number):
             return Pow(self, Number(other))
         else:
             return Pow(self, other)
 
     def __rpow__(self, other):
+        """Define reverse exponentiation of expressions."""
         if isinstance(other, numbers.Number):
             return Pow(Number(other), self)
         else:
-            raise TypeError(f"Unsupported operand type(s) for **: '{type(other).__name__}' and '{type(self).__name__}'")
+            raise TypeError(
+                f"Unsupported operand type(s) for **: '{type(other).__name__}'\
+                and '{type(self).__name__}'")
 
 
 class Operator(Expression):
+    """Define the Operator subclass of Expression."""
 
     def __repr__(self):
+        """Implement representation of expressions."""
         return type(self).__name__ + repr(self.operands)
 
     def __str__(self):
+        """Implement string representation of expressions."""
         str_operands = []
         for o in self.operands:
             if o.precedence < self.precedence:
@@ -91,47 +118,64 @@ class Operator(Expression):
 
 
 class Add(Operator):
+    """Define the Add subclass of Operator."""
+
     precedence = 0
     symbol = ' + '
 
 
 class Sub(Operator):
+    """Define the Subtract subclass of Operator."""
+
     precedence = 0
     symbol = ' - '
 
 
 class Mul(Operator):
+    """Define the Multiply subclass of Operator."""
+
     precedence = 1
     symbol = ' * '
 
 
 class Div(Operator):
+    """Define the Division subclass of Operator."""
+
     precedence = 1
     symbol = ' / '
 
 
 class Pow(Operator):
+    """Define the Power subcalss of Operator."""
+
     precedence = 2
     symbol = ' ^ '
 
 
 class Terminal(Expression):
+    """Define the Terminal sublass of Operator."""
 
     precedence = 3
 
     def __init__(self, value):
+        """Initialise."""
         self.value = value
         super().__init__()
 
     def __repr__(self):
+        """Implement representation of Terminals."""
         return repr(self.value)
 
     def __str__(self):
+        """Implement string representation of Terminals."""
         return str(self.value)
 
+
 class Symbol(Terminal):
+    """Define Symbol subclass of Terminal."""
 
     def __init__(self, value):
+        """Initialize."""
         if not isinstance(value, str):
             raise TypeError('Symbol must take a string')
         else:
@@ -139,8 +183,10 @@ class Symbol(Terminal):
 
 
 class Number(Terminal):
+    """Define Number subclass of Terminal."""
 
     def __init__(self, value):
+        """Initialize."""
         if not isinstance(value, numbers.Number):
             raise TypeError('Number must take a number')
         else:
@@ -148,6 +194,10 @@ class Number(Terminal):
 
 
 def postvisitor(expr, fn, **kwargs):
+    """Visit nodes of a tree in post order.
+
+    Execute a function, visiting each node only once.
+    """
     stack = []
     visited = {}
     stack += [expr]
@@ -192,6 +242,7 @@ def differentiate(expr, *o, **kwargs):
 def _(expr, *o, **kwargs):
     return 0
 
+
 @differentiate.register(Number)
 def _(expr, *o, **kwargs):
     return 0
@@ -219,11 +270,13 @@ def _(expr, *o, **kwargs):
 def _(expr, *o, **kwargs):
     return expr.operands[0] * o[1] + expr.operands[1] * o[0]
 
+
 @differentiate.register(Div)
 def _(expr, *o, **kwargs):
     num = expr.operands[1] * o[0] - expr.operands[0] * o[1]
     denom = expr.operands[1] ** 2
     return num / denom
+
 
 @differentiate.register(Pow)
 def _(expr, *o, **kwargs):
